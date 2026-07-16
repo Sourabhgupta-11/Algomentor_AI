837829d684b4146923f40090ac97bab1245fdaee
Sourabh Gupta <sourabhgupta.1331@gmail.com>
2026-07-17 03:39:08 +0530

    Set up react app for frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useRef, useState } from "react";
+import "./App.css";
+
+
+export default function App() {
+
+  return (
+    <div className="app-shell">
+  
+    </div>
+  );
+}
